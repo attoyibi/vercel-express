5e6548d3754d229c9601b87acd497db6883215fc
attoyibi <52569071+muhammad-muchson@users.noreply.github.com>
2023-10-04 16:47:04 +0700

    Update login.route.js

--- routes/login.route.js
+++ routes/login.route.js
@@ -7,19 +7,19 @@ r.get("/", (req, res) =>
   res.json(new SuccessResponseObject("login path live 🚀"))
 );
 // Routing untuk login
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
+// app.post("/login", (req, res) => {
+//   const { username, password } = req.body;
 
-  // Di sini Anda dapat menambahkan logika autentikasi pengguna.
-  // Contoh sederhana:
-  if (username === "user" && password === "password") {
-    // Jika login berhasil, buat token JWT
-    const token = jwt.sign({ username }, "secret_key"); // Ganti 'secret_key' dengan kunci rahasia yang lebih kuat
+//   // Di sini Anda dapat menambahkan logika autentikasi pengguna.
+//   // Contoh sederhana:
+//   if (username === "user" && password === "password") {
+//     // Jika login berhasil, buat token JWT
+//     const token = jwt.sign({ username }, "secret_key"); // Ganti 'secret_key' dengan kunci rahasia yang lebih kuat
 
-    // Kirim token sebagai respons
-    res.json({ message: "Login berhasil", token });
-  } else {
-    res.status(401).json({ message: "Login gagal" });
-  }
-});
+//     // Kirim token sebagai respons
+//     res.json({ message: "Login berhasil", token });
+//   } else {
+//     res.status(401).json({ message: "Login gagal" });
+//   }
+// });
 module.exports = r;
