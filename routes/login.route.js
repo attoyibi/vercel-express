1f43622e3af30597f4a704827cd0b5a85210a811
attoyibi <52569071+muhammad-muchson@users.noreply.github.com>
2023-10-04 16:56:29 +0700

    Update login.route.js

--- routes/login.route.js
+++ routes/login.route.js
@@ -8,17 +8,18 @@ r.get("/", (req, res) =>
 );
 
 // Rute untuk login (POST)
-r.post("/login", (req, res) => {
+r.post("/", (req, res) => {
   // Di sini Anda dapat menambahkan logika autentikasi pengguna.
   // Contoh sederhana:
   const { username, password } = req.body;
+  res.json(new SuccessResponseObject("login post path live 🚀"));
 
-  if (username === "user" && password === "password") {
-    // Jika login berhasil, Anda dapat mengirim respons sukses
-    res.json(new SuccessResponseObject("Login berhasil"));
-  } else {
-    // Jika login gagal, Anda dapat mengirim respons gagal
-    res.status(401).json(new ErrorResponseObject("Login gagal"));
-  }
+  //   if (username === "user" && password === "password") {
+  //     // Jika login berhasil, Anda dapat mengirim respons sukses
+  //     res.json(new SuccessResponseObject("Login berhasil"));
+  //   } else {
+  //     // Jika login gagal, Anda dapat mengirim respons gagal
+  //     res.status(401).json(new ErrorResponseObject("Login gagal"));
+  //   }
 });
 module.exports = r;
