2438c439c1c0ed004d75cc6eddd687e172ecd63c
attoyibi <52569071+muhammad-muchson@users.noreply.github.com>
2023-10-04 16:48:10 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
 const { SuccessResponseObject } = require("../common/http");
 const demo = require("./demo.route");
-const demo = require("./login.route");
+const login = require("./login.route");
 
 const r = Router();
 
 r.use("/demo", demo);
-r.use("/login", demo);
+r.use("/login", login);
 
 r.get("/", (req, res) =>
   res.json(new SuccessResponseObject("express vercel boiler plate"))
